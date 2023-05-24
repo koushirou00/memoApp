0fa89592edb72ec9f234482f8047d7961061fb67
koushirou00 <130426310+koushirou00@users.noreply.github.com>
2023-05-24 19:18:45 +0900

    ファイルへのコメントアウト完了

--- preload.js
+++ preload.js
@@ -1,28 +1,27 @@
+// Electronから必要なモジュールや関数をインポート
 const { contextBridge, ipcRenderer } = require('electron');
 
+// メインプロセスとレンダラープロセスの間で通信するために、IPCハンドラーを公開する
 contextBridge.exposeInMainWorld('ipc', {
   saveFile: async (content, filePath) => {
-    //ipcRenderer.invokeでメインプロセス（main.js）にデータ送信
-    //チャンネル名（第一引数）:「'save-file'」
-    //
-    await ipcRenderer.invoke('save-file', content, filePath);
+    await ipcRenderer.invoke('save-file', content, filePath); // メインプロセスにファイルの内容を保存するように指示する
   },
   saveFileAs: async (content) => {
-    await ipcRenderer.invoke('save-file-as', content);
+    await ipcRenderer.invoke('save-file-as', content); // メインプロセスに新しいファイルとしてファイルの内容を保存するように指示する
   },
   openFile: async (filePath) => {
-    return await ipcRenderer.invoke('open-file', filePath);
+    return await ipcRenderer.invoke('open-file', filePath); // 指定されたファイルを開いてその内容を返すようにメインプロセスに指示する
   },
   on: (channel, callback) => {
-    ipcRenderer.on(channel, callback);
+    ipcRenderer.on(channel, callback); // 指定されたチャネルでIPCイベントを受信するようにレンダラープロセスに指示する
   },
   once: (channel, callback) => {
-    ipcRenderer.once(channel, callback);
+    ipcRenderer.once(channel, callback); // 指定されたチャネルで一度だけIPCイベントを受信するようにレンダラープロセスに指示する
   },
   removeListener: (channel, callback) => {
-    ipcRenderer.removeListener(channel, callback);
+    ipcRenderer.removeListener(channel, callback); // 指定されたチャネルからIPCリスナーを削除するようにレンダラープロセスに指示する
   },
   removeAllListeners: (channel) => {
-    ipcRenderer.removeAllListeners(channel);
+    ipcRenderer.removeAllListeners(channel); // 指定されたチャネルのすべてのIPCリスナーを削除するようにレンダラープロセスに指示する
   },
 });
